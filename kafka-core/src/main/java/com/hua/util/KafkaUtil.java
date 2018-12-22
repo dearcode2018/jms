dfd9efb53b053b6a7c23fab0ca49ee1510260cea
qianye.zheng <dearzqy@163.com>
2018-12-22 13:42:16 +0800

    no message

---
+++ kafka-core/src/main/java/com/hua/util/KafkaUtil.java
@@ -0,0 +1,25 @@
+/**
+ * KafkaUtil.java
+ * @author  qye.zheng
+ * 	version 1.0
+ */
+package com.hua.util;
+
+/**
+ * KafkaUtil
+ * 描述: 
+ * @author  qye.zheng
+ */
+public final class KafkaUtil
+{
+
+	/**
+	 * 构造方法
+	 * 描述: 
+	 * @author  qye.zheng
+	 */
+	private KafkaUtil()
+	{
+	}
+
+}
